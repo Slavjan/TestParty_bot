97d3253301b6e07d98796c3ab4d1080c30b72706
Yaroslav Khaidayev <khaidayev@gmail.com>
2022-08-08 02:59:43 +0700

    get message json obj

--- TestParty_bot.py
+++ TestParty_bot.py
@@ -10,14 +10,19 @@ token='5458673070:AAFfRWx8DdfK-z4M-z5Bj_GKBN6WxGZOWqA'
 
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
+###/***
 def start_message(message):
-    bot.send_message(message.chat.id,'здарова заибал!')
+    mess = f'здарова {message.from_user.first_name}'
+    bot.send_message(message.chat.id, mess)
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+###***/    
 @bot.message_handler(commands=['button'])
-def button_message(message):
+def button_message(message): ###/*
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Кнопка")
     markup.add(item1)
     bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+###*/    
 @bot.message_handler(content_types='text')
 def message_reply(message):
     if message.text=="Кнопка":
